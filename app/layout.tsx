a6789bb2b5dcf4d5ce311256ddef8df4c6317d0e
Charlie Joseph <ch@rli.es>
2024-06-12 12:22:56 +0100

    WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,62 +1,62 @@
 import "./css/style.css";
 
-import { Inter, Inter_Tight } from "next/font/google";
-import Theme from "./theme-provider";
+import {Inter, Inter_Tight} from "next/font/google";
 import {Metadata} from "next";
 import Fathom from "@/components/fathom";
+import Analytics from "@/components/Analytics";
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
+    subsets: ["latin"],
+    variable: "--font-inter",
+    display: "swap",
 });
 
 const inter_tight = Inter_Tight({
-  weight: ["500", "600", "700"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  variable: "--font-inter-tight",
-  display: "swap",
+    weight: ["500", "600", "700"],
+    style: ["normal", "italic"],
+    subsets: ["latin"],
+    variable: "--font-inter-tight",
+    display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Gamertag.com - The Ultimate Gaming Domain",
-  description:
-      "Gamertag.com is one of the most iconic gaming domains. Own the brand that speaks to millions of gamers worldwide.",
-  themeColor: "#111C2C",
-  alternates: {
-    canonical: "https://gamertag.com",
-  },
-  icons: [
-    {
-      rel: 'icon',
-      type: 'image/x-icon',
-      url: '/icon-light.png',
-      media: '(prefers-color-scheme: light)',
+    title: "Gamertag.com - The Ultimate Gaming Domain",
+    description:
+        "Gamertag.com is one of the most iconic gaming domains. Own the brand that speaks to millions of gamers worldwide.",
+    themeColor: "#111C2C",
+    alternates: {
+        canonical: "https://gamertag.com",
     },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      url: '/icon-dark.png',
-      media: '(prefers-color-scheme: dark)',
-    },
-  ],
+    icons: [
+        {
+            rel: 'icon',
+            type: 'image/x-icon',
+            url: '/icon-light.png',
+            media: '(prefers-color-scheme: light)',
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            url: '/icon-dark.png',
+            media: '(prefers-color-scheme: dark)',
+        },
+    ],
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      {/* suppressHydrationWarning: https://github.com/vercel/next.js/issues/44343 */}
-      <body className={`${inter.variable} ${inter_tight.variable} font-inter antialiased bg-indigo-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200 tracking-tight`}>
-        <Fathom/>
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <Analytics/>
+        {/* suppressHydrationWarning: https://github.com/vercel/next.js/issues/44343 */}
+        <body className={`${inter.variable} ${inter_tight.variable} font-inter antialiased bg-indigo-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200 tracking-tight`}>
         <div className="relative flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
-          {children}
+            {children}
         </div>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
