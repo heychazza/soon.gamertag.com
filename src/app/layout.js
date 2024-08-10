23a5e4d50defdf17771e8ac3b29e35536b52b6b7
Charlie Joseph <ch@rli.es>
2024-08-10 21:38:18 +0100

    Fix antialiased

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Analytics/>
-      <body className={protoMono.className}>{children}</body>
+      <body className={`${protoMono.className} antialiased`}>{children}</body>
     </html>
   );
 }
